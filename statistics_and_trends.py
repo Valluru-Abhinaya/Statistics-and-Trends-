141a4a310ce66e78e3fe63506b86c79e0e71a430
Valluru Abhinaya <abhinayavalluru10@gmail.com>
2025-03-06 11:47:40 +0000

    Update statistics_and_trends.py

--- statistics_and_trends.py
+++ statistics_and_trends.py
@@ -74,7 +74,8 @@ def writing(moments, col):
         f'Mean = {moments[0]:.2f}, '
         f'Standard Deviation = {moments[1]:.2f}, '
         f'Skewness = {moments[2]:.2f}, and '
-        f'Excess Kurtosis = {moments[3]:.2f}.')    
+        f'Excess Kurtosis = {moments[3]:.2f}.'
+    )    
     skew_desc = "not skewed" if abs(moments[2]) < 2 else (
         "right skewed" if moments[2] > 0 else "left skewed"
     )
